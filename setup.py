aca90ce52e08a130f7ca71ea6a71b2bb294f8395
alex.blaessle <alexander.blaessle@gmail.com>
2016-03-04 01:25:32 +0100

    initial commit/adjusted

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyfrp',
+      version='1.0',
+      description='PyFRAP: A Python based FRAP analysis tool box',
+      url='https://github.com/alexblaessle/PyFRAP',
+      author='Alexander Blaessle',
+      author_email='alexander.blaessle@tuebingen.mpg.de',
+      license='GNU GPL Version 3',
+      packages=['pyfrp'],
+      zip_safe=False)
