a5e70a73c51e14b8b45b626cd25d4b63b465f030
alex.blaessle <alexander.blaessle@gmail.com>
2016-03-06 12:05:40 +0100

    removed unnecessary folders from git

--- pyfrp/scripts/definepath.py
+++
@@ -1,5 +0,0 @@
-### script if path definition works
-
-import pyfrp_gmsh_module
-
-print pyfrp_gmsh_module.getGmshBin()
--- pyfrp/scripts/testscripts/testTerminal.py
+++
@@ -1,5 +0,0 @@
-###Script to test terminal
-
-import pyfrp_term
-
-import pyqt
