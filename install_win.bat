7ec4002a1f502e311af428e49e36e68982476c5d
mueller-lab <32100660+mueller-lab@users.noreply.github.com>
2018-03-24 22:04:40 +0100

    Add files via upload

--- install_win.bat
+++ install_win.bat
@@ -1,4 +1,4 @@
-REM This script installs all dependencies of PyFRAP for OSX, assuming Anaconda is installed.
+REM This script installs all dependencies of PyFRAP for Windows, assuming Anaconda and the JDK are installed.
 
 REM Downgrade to PyQt4
 conda remove -y qt pyqt
@@ -8,20 +8,17 @@ REM Reinstall MPL
 conda remove -y pywavelets
 conda install -y matplotlib=1.4.3
 
-REM Reinstall skimage
-conda install -y scikit-image=0.11.3
-
-REM Install extra packages
-conda install -y vtk
-conda install -y colorama
-
 REM Install extra packages using 
+python -m pip install --upgrade pip
+pip install colorama
 pip install numpy-stl
 pip install solidpython
 pip install wget
-pip install pysparse
 pip install fipy
 pip install python-bioformats
 
 REM Install PyFRAP
 python setup.py install 
+
+REM Reinstall skimage and install vtk
+conda install -y scikit-image=0.11.3 vtk
